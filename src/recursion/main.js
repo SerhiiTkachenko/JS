479732590d31cd8220757d3be8a96ef4156abfed
Serhii <tkachenko.serhij@gmail.com>
2018-05-30 08:31:28 +0300

    add new function

--- src/recursion/main.js
+++ src/recursion/main.js
@@ -1,9 +1,20 @@
-const factorial = (n) => {
-    if(n === 1) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
+// const factorial = (n) => {
+//     if(n === 1) {
+//         return 1;
+//     } else {
+//         return n * factorial(n - 1);
+//     }
+// }
+
+// alert(factorial(3));
+
+const sequenceSum = (begin,end) => {
+    if (begin > end) {
+        return NaN;
+    }else if(begin === end){
+        return begin;
+    }else {
+        return begin + sequenceSum(begin + 1, end);
     }
 }
-
-alert(factorial(3));
+console.log(sequenceSum(-3,2));
