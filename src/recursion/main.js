b227e646f8e724bc3d825160cc329eac45995330
Serhii <tkachenko.serhij@gmail.com>
2018-05-30 12:01:18 +0300

    add new itaretive function

--- src/recursion/main.js
+++ src/recursion/main.js
@@ -7,17 +7,42 @@
 // }
 // alert(factorial(3));
 
+
+// iter
+
+// const factorial = (n) => {
+//     const iter = (current,acc) => {
+//         if (current === 0) {
+//             return acc;
+//         }
+//         return iter (current -1, acc * current);
+//     }
+//     return iter(n,1);
+// }
+// console.log(factorial(4));
 // sequenceSum
-const sequenceSum = (begin,end) => {
-    if (begin > end) {
-        return NaN;
-    }else if(begin === end){
-        return begin;
-    }else {
-        return begin + sequenceSum(begin + 1, end);
-    }
-}
-console.log(sequenceSum(-3,2));
+// const sequenceSum = (begin,end) => {
+//     if (begin > end) {
+//         return NaN;
+//     }else if(begin === end){
+//         return begin;
+//     }else {
+//         return begin + sequenceSum(begin + 1, end);
+//     }
+// }
+// console.log(sequenceSum(1,5));
+
+//fibonacci
+// let fibonacci = (n) => {
+//     // console.log(n);
+//     if(n < 2) {
+//         return n;
+//     } else {
+//         return (fibonacci(n-1) + fibonacci(n-2));
+//     }
+// }
+// console.log(fibonacci(2));
+
 // count
 // const count = (c) => {
 //     console.log(c);
@@ -26,5 +51,31 @@ console.log(sequenceSum(-3,2));
 //     } else {
 //         count(c - 1);
 //     }
-// } 
+// }
 // count(5);
+
+// smallestDivisor
+const smallestDivisor = (num) => {
+    const iter = (acc) => {
+        if(acc > num / 2){
+            return num;
+        } else if (num % acc === 0) {
+            return acc;
+        }
+        return iter(acc + 1);
+    }
+    return iter(2);
+};
+
+console.log(smallestDivisor(9));
+
+// const factorial = (n) => {
+//     const iter = (current,acc) => {
+//         if (current === 0) {
+//             return acc;
+//         }
+//         return iter (current -1, acc * current);
+//     }
+//     return iter(n,1);
+// }
+// console.log(factorial(4));
