6c673a12a6e8132aa01cdbbe9c2b9a4ecde8bfce
Serhii <tkachenko.serhij@gmail.com>
2018-05-31 12:26:40 +0300

    add new function smallestDivisor

--- src/loops/loop.js
+++ src/loops/loop.js
@@ -1,1 +1,30 @@
-alert('loop');
+// const factorial = (n) => {
+//     let counter = 1;
+//     let result = 1;
+
+//     while (counter <= n) {
+//         counter = counter + 1;
+//         result = result * counter;
+//     }
+//     return result;
+// }
+
+// console.log(factorial(2));
+
+const smallestDivisor = (num) => {
+    if (num < 1) {
+        return NaN;
+    }
+    if (num === 1) {
+        return num;
+    }
+
+    let divisor = 2;
+
+    while (num % divisor !== 0) {
+        divisor = divisor + 1;
+    }
+
+    return divisor;
+};
+console.log(smallestDivisor(121));
