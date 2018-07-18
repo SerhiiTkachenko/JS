bf8dfe2ca3fdd89a6167a7ffcd0ea2853acd6cb8
Serhii <tkachenko.serhij@gmail.com>
2018-07-18 16:49:00 +0300

    changed name of function and coment

--- src/AcademyStudy/JS/jscomponent/myapp.js
+++ src/AcademyStudy/JS/jscomponent/myapp.js
@@ -1,14 +1,14 @@
-// var sugar = prompt("how many spoon of sugar, do you want?");
-// var milk = prompt("how many percent of milk, do you want?");
+var sugar = prompt("how many spoon of sugar, do you want?");
+var milk = prompt("how many percent of milk, do you want?");
 
-// function makeCofee( sugar, milk ) {
-//     var instructions = "Boil water,"; 
-//     instructions += " pour into coup"; 
-//     instructions += " add coffee granules"; 
-//     instructions += " add coffee granules"; 
-//     instructions += " add "+ sugar + " spoons of sugar,"; 
-//     instructions += " add "+ milk + "% milk.";
-//     return instructions; 
-// }
-// document.write( makeCofee(sugar, milk) );
+function makeCoffee( sugar, milk ) {
+    var instructions = "Boil water,"; 
+    instructions += " pour into coup"; 
+    instructions += " add coffee granules"; 
+    instructions += " add coffee granules"; 
+    instructions += " add "+ sugar + " spoons of sugar,"; 
+    instructions += " add "+ milk + "% milk.";
+    return instructions; 
+}
+document.write( makeCoffee(sugar, milk) );
 
