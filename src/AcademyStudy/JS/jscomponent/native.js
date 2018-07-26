c3e8fd0c99ab4fc50d355d0ad6dd7af4c28757c6
Serhii <tkachenko.serhij@gmail.com>
2018-07-26 14:33:48 +0300

    added symbol and test

--- src/AcademyStudy/JS/jscomponent/native.js
+++ src/AcademyStudy/JS/jscomponent/native.js
@@ -4,8 +4,7 @@
 var message;
 message = 'Hello world';
 
-var name = 'Jhon',
-age = 24;
+var name = 'Jhon';
 
 //let
 
@@ -23,6 +22,39 @@ const user = {
     name: 'Jhon',
     age:24
 };
-
 const GRAY = '#ccc';
-/*TYPES*/
+
+/*STRING*/
+let str = "new string";
+
+let age = 23;
+let newStr = `age = ${age}`;
+console.log(newStr);
+
+console.log(`first line
+second line`);
+
+newStr = str + ' second string'; //New string second string same as .concat();
+console.log(newStr);
+
+console.log('_______________');
+console.log(str.length);
+console.log(str.charAt(2));
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str.indexOf('str'));
+console.log(newStr.substring(11, 17));//starting string/and end string
+console.log(newStr.substr(11, 6)); //starting string/and numbers of characters
+console.log('_______________');
+/*SYMBOL ES2015*/
+let isAdmin = Symbol("isAdmin");
+
+let userAdmin = {
+    name: "Boss",
+    [isAdmin]: true
+};
+
+console.log(userAdmin[isAdmin]);
+console.log(userAdmin.isAdmin);
+console.log(userAdmin);
+console.log('_______________');
